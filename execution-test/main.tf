c97c1096857ebdd2482f466f0c03c4b60802f44c
hrudayanandkaira <46514955+hrudayanandkaira@users.noreply.github.com>
2023-04-05 19:41:24 +0530

    Update main.tf

--- execution-test/main.tf
+++ execution-test/main.tf
@@ -50,7 +50,7 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = [ "eu-west-1"]
+  azs             = [ "us-west-1a"]
   private_subnets = ["10.0.1.0/24"]
   public_subnets  = ["10.0.2.0/24"]
 
