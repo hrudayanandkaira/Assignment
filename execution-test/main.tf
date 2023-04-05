0e6977c54a92b8e1bf785557ad666658f4e280a1
hrudayanandkaira <46514955+hrudayanandkaira@users.noreply.github.com>
2023-04-05 20:00:56 +0530

    Update main.tf

--- execution-test/main.tf
+++ execution-test/main.tf
@@ -78,3 +78,15 @@ module "ec2_instance" {
     Environment = "dev"
   }
 }
+ ###################################s3 bucket creation################ 
+  module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
