14ad9d165cb3708d1e379d86d8de3aa58e8e8b50
hrudayanandkaira <46514955+hrudayanandkaira@users.noreply.github.com>
2023-04-05 15:08:49 +0530

    Update main.tf

--- execution-test/main.tf
+++ execution-test/main.tf
@@ -1,1 +1,19 @@
 
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-instance"
+
+  ami                    = "ami-01d9361b1c190a61b"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-077b363f385bf4006"]
+  subnet_id              = "subnet-0c9f8b746957b6e2b"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
