7585d0f5d1827e99a4a203a311d3af8d2eebd12f
hrudayanandkaira <46514955+hrudayanandkaira@users.noreply.github.com>
2023-04-05 18:16:29 +0530

    Update main.tf

--- execution-test/main.tf
+++ execution-test/main.tf
@@ -10,6 +10,41 @@ terraform {
 provider "aws" {
   region = "us-west-1"
 }
+module "ec2_security_group" {
+  source                     = "../../sg"
+  security_group_name        = "first_SG"
+  security_group_description = "This is the dev security grp for Atlassian Bamboo"
+  tags = {
+    Name = "EQ_AWS_Dev_Atlassian_Bamboo"
+  }
+}
+
+module "ec2_rule" {
+  source            = "../../sg/sg_rules"
+  security_group_id = module.ec2_security_group.sg_id
+  security_group_rule = [
+
+    {
+      from_port   = 54663
+      to_port     = 54663
+      cidr_blocks = ["172.31.0.1/32"] #need to be changed
+      protocol    = "tcp"
+      type        = "ingress"
+      description = "Agent Server to Bamboo Server"
+    },
+    {
+      from_port   = 54663
+      to_port     = 54663
+      cidr_blocks = ["172.31.0.1/32"] #need to be changed
+      protocol    = "tcp"
+      type        = "ingress"
+      description = "Agent Server to Bamboo Server"
+    },
+  ]
+}
+
+
+
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
@@ -17,7 +52,7 @@ module "ec2_instance" {
   ami                    = "ami-01d9361b1c190a61b"
   instance_type          = "t2.micro"
   monitoring             = true
-  vpc_security_group_ids = ["sg-077b363f385bf4006"]
+  vpc_security_group_ids = module.ec2_security_group.sg_id
   subnet_id              = "subnet-0c9f8b746957b6e2b"
 
   tags = {
