0a4d1219d653db1287bc13838c923587afad08cb
hrudayanandkaira <46514955+hrudayanandkaira@users.noreply.github.com>
2023-04-05 17:59:36 +0530

    Create main.tf

---
+++ Security_group/main.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "security_group" {
+  name        = var.security_group_name
+  name_prefix = var.security_group_name_prefix
+  description = var.security_group_description
+  vpc_id      = var.vpc_id
+  # revoke_rules_on_delete 
+  dynamic "ingress" {
+    for_each = var.ingress == [] ? [] : var.ingress
+    content {
+      description      = lookup(ingress.value, "description", null)
+      from_port        = lookup(ingress.value, "from_port", null)
+      to_port          = lookup(ingress.value, "to_port", null)
+      protocol         = lookup(ingress.value, "protocol", null)
+      cidr_blocks      = lookup(ingress.value, "cidr_blocks", null)
+      ipv6_cidr_blocks = lookup(ingress.value, "ipv6_cidr_blocks", null)
+      prefix_list_ids  = lookup(ingress.value, "prefix_list_ids", null)
+      security_groups  = lookup(ingress.value, "security_groups", null)
+      self             = lookup(ingress.value, "self", null)
+    }
+  }
+
+  dynamic "egress" {
+    for_each = var.egress
+    content {
+      description      = lookup(egress.value, "description", null)
+      from_port        = lookup(egress.value, "from_port", null)
+      to_port          = lookup(egress.value, "to_port", null)
+      protocol         = lookup(egress.value, "protocol", null)
+      cidr_blocks      = lookup(egress.value, "cidr_blocks", null)
+      ipv6_cidr_blocks = lookup(egress.value, "ipv6_cidr_blocks", null)
+      prefix_list_ids  = lookup(egress.value, "prefix_list_ids", null)
+      security_groups  = lookup(egress.value, "security_groups", null)
+      self             = lookup(egress.value, "self", null)
+    }
+  }
+
+  tags = var.tags
+}
