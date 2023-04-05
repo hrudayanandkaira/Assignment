5b9f33e79b5f10a22bf678a1444dc1c4ebb203fc
hrudayanandkaira <46514955+hrudayanandkaira@users.noreply.github.com>
2023-04-05 16:29:35 +0530

    Update main.tf

--- execution-test/main.tf
+++ execution-test/main.tf
@@ -18,7 +18,7 @@ module "ec2_instance" {
 
   ami                    = "ami-01d9361b1c190a61b"
   instance_type          = "t2.micro"
-  key_name               = "user1"
+  #key_name               = "user1"
   monitoring             = true
   vpc_security_group_ids = ["sg-077b363f385bf4006"]
   subnet_id              = "subnet-0c9f8b746957b6e2b"
